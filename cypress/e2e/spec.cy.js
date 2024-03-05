1a4a8021216f56f58210123ef6bc81d1baf85c41
makkar-nishant123 <makkar.nishant123@gmail.com>
2024-03-05 09:56:34 -0800

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,20 +2,35 @@ require('@cypress/xpath');
 
 describe('template spec', () => {
   it('Validate Phrase Search Results.', () => {
-    const searchphrase = "Internet"
-    cy.visit('https://telus.com')
-    cy.get("#close-cookies-notice-banner").click()
-    cy.xpath(".//*[@aria-label='Navigation Menu']").click()
-    cy.xpath("(.//*[@placeholder='Search TELUS.com'])[2]").type(searchphrase) //Search phrase.
-    cy.xpath("(.//*[@data-test='searchResultItem']/a)[8]").contains(searchphrase)
+    const searchphrase = "Internet" //Defining search phrase here.
     
+    cy.visit('https://telus.com') //Visit telus.com
+    cy.wait(2000)
+    
+    cy.get("#close-cookies-notice-banner").click() //Accept Cookies
+    
+    cy.xpath(".//*[@aria-label='Navigation Menu']").click() // Select Navigation Menu.
+    
+    cy.xpath("(.//*[@placeholder='Search TELUS.com'])[2]").type(searchphrase) //Search your phrase.
+    
+    // Make sure your third element contains your search phrase.
+    cy.xpath("(.//*[@data-test='searchResultItem']/a)[8]").contains(searchphrase) 
+    
+    //Get search result text and click on it.
     cy.xpath("(.//*[@data-test='searchResultItem']/a)[8]").then($value => {
       const searchphraseresult = $value.text()
       cy.wrap(searchphraseresult).as('wrapValue')
+
+      //Search result text must match your selected value.
       cy.xpath("(.//*[@data-test='searchResultItem']/a)[8]").click()
       cy.xpath(".//*[contains(text(),'Search results for')]").contains(searchphraseresult)
      
   })
+
+  //Make sure you have more than 6 results.
+  cy.xpath(".//*[contains(@class, \"styles__ListItem-sc\")]/a").its('length').should('be.gte', 6)
+
+  //Generate a random number and click random search result.
   let x =  Math.floor(Math.random() * 10)
   cy.xpath("(.//*[contains(@class, \"styles__ListItem-sc\")]/a)[" + x + "]").click()
 
